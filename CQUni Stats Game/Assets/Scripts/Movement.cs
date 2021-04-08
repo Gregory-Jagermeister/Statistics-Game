0178851be5c4bcf32796a2aa839d2e7cec00421d
Daniel Stirba <danielstirba97@gmail.com>
2021-04-08 15:13:47 +1000

    Changed to use the physics system properly

--- CQUni Stats Game/Assets/Scripts/Movement.cs
+++ CQUni Stats Game/Assets/Scripts/Movement.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private Rigidbody2D player;
     private Vector3 velocity;
     public float speed = 10f;
 
+    private void Start()
+    {
+        player = this.GetComponent<Rigidbody2D>();
+    }
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //set velocity to equal a new Vector3 based on input on the Horizontal and Vertical axises as the x and y values and a z vaule of 0. 
         //Normalize this vector then multiply the x and y values by the speed variable to define movemnet speed. 
         //Finally use this vector to translate the transform of this gameObject
 
-        velocity = new Vector3(Input.GetAxis("Horizontal") ,Input.GetAxis("Vertical"),0);
-        velocity.Normalize();
-        velocity.x = velocity.x * speed;
-        velocity.y = velocity.y * speed;
-        transform.Translate( velocity * Time.deltaTime);
+        velocity = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        //velocity.Normalize();
+        //velocity.x = velocity.x * speed;
+        //velocity.y = velocity.y * speed;
+        //transform.Translate( velocity * Time.deltaTime);
+
+        //Daniels Edit
+        player.MovePosition(transform.position + velocity * Time.deltaTime * speed);
+
     }
 }
