c7ae48fdf354c3b08ccafe68dcf0b5729f773402
LaithAlfadhli <laith.alfadhli@students.mq.edu.au>
2021-04-19 21:12:31 +1000

    implemented multi choice and input quiz questions

---
+++ CQUni Stats Game/Assets/Scripts/Quiz/Questions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Questions 
+{
+  public string question;
+  public string[] answers;
+  public int correctAnswer;
+  public bool isMultiChoice;
+}
