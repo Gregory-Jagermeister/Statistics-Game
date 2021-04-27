0367b71b653bcbc0e2c878d2798406a4adaba6d2
Lorenzler <61770649+Lorenzler@users.noreply.github.com>
2021-04-27 20:23:51 +1000

    tidying code

--- CQUni Stats Game/Assets/Scripts/Interactable.cs
+++ CQUni Stats Game/Assets/Scripts/Interactable.cs
@@ -12,9 +12,7 @@ public class Interactable : MonoBehaviour
     public ArtifactMenu menu;
     public bool isProf = false;
     public string sceneNameTransition = "Quiz";
-    //public float timer = 0.0f;
-    public float test = 0.0f;
-    public string send;
+
 
     [SerializeField]
     private string BASE_URL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdKmNRxpf0460uiCSEKMoheZodlUqtHkM8MCAEy4fGS3y_d-A/formResponse";
@@ -29,22 +27,13 @@ public class Interactable : MonoBehaviour
         }
     }
 
-    IEnumerator Create(string timer)
-    {
-        WWWForm form = new WWWForm();
-        form.AddField("entry.172307503", timer);
-        byte[] rawData = form.data;
-        WWW www = new WWW(BASE_URL, rawData);
-        yield return www;
-    }
 
     // Update is called once per frame
     void Update()
     {
         
         
-        //Statics.timer += Time.deltaTime;
-        test = Statics.timer;
+        
         //if player is close enough, can click the trigger key to trigger a message say ("you clicked on an artifact")
         if (Vector3.Distance(player.transform.position, transform.position) < radius)
         {
@@ -52,8 +41,7 @@ public class Interactable : MonoBehaviour
             {
                 if (isProf)
                 {
-                    //send = Statics.timer.ToString();
-                    //StartCoroutine(Create(send));
+                    
 
 
                     SceneManager.LoadScene(sceneNameTransition);
