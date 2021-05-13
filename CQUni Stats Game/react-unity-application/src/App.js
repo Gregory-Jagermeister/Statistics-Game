a1e3d280f25aab5f161e1443d7a62ffb8631b7ad
Daniel Stirba <danielstirba97@gmail.com>
2021-05-13 21:08:12 +1000

    Added Unity Game to react app, Lots of errors

--- CQUni Stats Game/react-unity-application/src/App.js
+++ CQUni Stats Game/react-unity-application/src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Unity, { UnityContent } from "react-unity-webgl";
+
+
 
 function App() {
+
+  const unityContext = new UnityContent('UnityBuild/Build/UnityBuild.json', 'UnityBuild/Build/UnityLoader.js');
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Unity unityContent={unityContext}></Unity>
       </header>
     </div>
   );
