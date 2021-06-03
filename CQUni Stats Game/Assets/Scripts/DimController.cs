077529f2c6deaf5cbf7b8aae1b4b20081b77835b
camillatea <61532941+camillatea@users.noreply.github.com>
2021-06-03 10:17:39 +1000

    Added Dimming Stuff

---
+++ CQUni Stats Game/Assets/Scripts/DimController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DimController : MonoBehaviour
+{
+    public Text txt;
+    public Image imgProf;
+    public Image imgBob;
+    private Color32 c;
+    private Color32 full;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        c = new Color32(77,77,77,255);
+        full = new Color32(255,255,255,255);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (txt.text == "Bob") {
+            imgBob.color = full;
+            imgProf.color = c;
+        }
+
+        if (txt.text == "The Professor") {
+            imgBob.color = c;
+            imgProf.color = full;
+        }
+    }
+}
