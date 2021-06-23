5eadc5247440765bd40a4ae24e6b7d63ed86dd42
Daniel Stirba <danielstirba97@gmail.com>
2021-06-24 00:34:21 +1000

    Fixed Game Aspect Ratio and made website look pretty

--- CQUni Stats Game/react-unity-application/src/App.js
+++ CQUni Stats Game/react-unity-application/src/App.js
@@ -1,19 +1,35 @@
 import './App.css';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 import Unity, { UnityContent } from "react-unity-webgl";
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { confirmAlert } from 'react-confirm-alert';
+
+function Game({unityContext, handleOnClickFullscreen}) {
+  return (
+    <>
+      <div className="GameContainer">
+        <Unity
+          className='Game'
+          unityContent={unityContext}>
+        </Unity>
+      </div>
+      <button onClick={handleOnClickFullscreen} className="FullScreen btn btn-info btn-lg">
+        <span className="glyphicon glyphicon-fullscreen"></span> Fullscreen
+      </button>
+    </>
+    );
+}
 
 function App() {
 
+  const [hasConfirmed, setHasConfirmed] = useState(false)
   const unityContext = new UnityContent('UnityBuild/Build/UnityBuild.json', 'UnityBuild/Build/UnityLoader.js');
   
   useEffect(() => {
-    unityContext.on("canvas", function (canvas) {
-      canvas.width = 100;
-      canvas.height = 50;
-    });
     window.onbeforeunload = confirmExit;
     function confirmExit()
     {
+      unityContext.send("GameController", "TestReactJS");
       return "show warning";
     }
   }, [])
@@ -29,12 +45,25 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <button onClick={handleOnClickFullscreen}>Fullscreen</button>
-        <Unity unityContent={unityContext} matchWebGLToCanvasSize={false}></Unity>
-      </header>
+        {hasConfirmed ?  <header className="App-header"><Game unityContext={unityContext} handleOnClickFullscreen={handleOnClickFullscreen}/></header> : confirmAlert({
+        title: 'Disclaimer:',
+        message: ' ',
+        childrenElement: () => <p>Please note that game play will be recorded for the purpose of third party research. Such game play pertains to:<br/><br/>- Interactions with exhibits <br/>- Quiz answers <br/>- Dialogue choices <br/><br/>Be advised that at no point will the game ask for your personal details and/or sensitive information. If you are uncomfortable with the above information being recorded please exit the game.</p>,
+          buttons: [
+          {
+            label: 'I Agree',
+            onClick: () => setHasConfirmed(true)
+          },
+          {
+            label: 'I Disagree',
+            onClick: () => window.location.reload()
+          }
+        ]
+        })}        
     </div>
   );
+
+
 }
 
 export default App;
